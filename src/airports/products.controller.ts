3db42d32b6b16b29347fc259df30515d41377c65
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-20 19:28:28 +0300

    mongo db add

--- src/airports/products.controller.ts
+++ src/airports/products.controller.ts
@@ -6,18 +6,50 @@ import {
   Body,
   Delete,
   Put,
-  Redirect,
   HttpCode,
   HttpStatus,
   Header,
-  Req,
-  Res,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
 
 @Controller('products')
 export class ProductsController {
+
+  constructor(private readonly productService: ProductsService) {
+  }
+
+  @Get()
+  getAll(): Promise<Product[]> {
+    return this.productService.getAll();
+  }
+
+  @Get(':id')
+  getId(@Param('id') id: string): Promise<Product> {
+    return this.productService.getById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)                   //вместо цифр можно указать статус фразой
+  @Header('Cache-Control', 'none')                //для создания headera
+  create(@Body() createProductDto: CreateProductDto): Promise<Product> {
+    return this.productService.create(createProductDto);
+    // return `Title : ${createProductDto.title} Price : ${createProductDto.price} `;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<Product> {
+    return this.productService.remove(id)
+  }
+
+  @Put(':id')
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return this.productService.update(id, updateProductDto)
+  }
+
+}
 
   // @Get()
   // @Redirect('https://google.com', 301)             //для редиректа
@@ -26,33 +58,3 @@ export class ProductsController {
   //   req.method.
   //   return 'get all airports';
   // }
-
-  @Get()
-  getAll(): string {
-    return 'GET all products';
-  }
-
-  @Get(':id')
-  getId(@Param('id') id: string): string {
-    return 'getId ' + id;
-  }
-
-  @Post()
-  @HttpCode(HttpStatus.CREATED)                   //вместо цифр можно указать статус фразой
-  @Header('Cache-Control', 'none')                //для создания headera
-  create(@Body() createProductDto: CreateProductDto): string {
-    return `Title : ${createProductDto.title} Price : ${createProductDto.price} `;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return 'Remove ' + id;
-  }
-
-  @Put(':id')
-  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
-    return 'Update ' + id;
-  }
-
-
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './airports/products.controller';
-import { ProductsService } from './airports/products.service';
+import { ProductsModule } from './airports/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot("mongodb+srv://webdotg:zxcasdqwe321zxc@first.v5ufhia.mongodb.net/productbox?retryWrites=true&w=majority")
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
